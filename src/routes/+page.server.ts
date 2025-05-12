0433724fe77adf10f15b8ea6be58141dc2e3cf39
SorenRemboll <soren.remboll@gmail.com>
2025-05-12 22:18:19 +0200

    initialize

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,17 @@
+import { prisma } from '$lib/prisma';
+import type { PageServerLoad } from './$types';
+
+
+export const load:PageServerLoad = async ({}) => {
+    const user = await prisma.user.findFirst();
+    if(!user) {
+        return {
+            user: null
+        }
+    }else{
+        return {
+            user: user
+        }
+    }
+	
+};
