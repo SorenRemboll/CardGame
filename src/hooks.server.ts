0ff4e4def532c858ca39c6ebf3c5d9f7e81ac0c1
SorenRemboll <soren.remboll@gmail.com>
2025-06-02 22:49:39 +0200

    layout and auth prep

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,5 +4,6 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve, }) => {
     // This is a workaround for the devtools issue
 	if (event.url.pathname.startsWith('/.well-known/appspecific/com.chrome.devtools')) {return new Response(null, { status: 204 })}
+
     return await resolve(event);
 };
---
+++ src/routes/[user_id]/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    const { children } = $props();
+
+</script>
+<div>
+    <p>Hi mom</p>
+    {@render children?.()}
+</div>
